5784c81823b31666cb789c980286a994d487e8d1
Facaller <ashwin.metal13@gmail.com>
2026-04-12 17:31:01 +0200

    Check coordinates

--- src/logic/gameboard/Gameboard.js
+++ src/logic/gameboard/Gameboard.js
@@ -1,10 +1,10 @@
 export class GameBoard {
     constructor () {
-        const rows = 24;
-        const cols = 24;
+        this.rows = 24;
+        this.cols = 24;
 
-        this.grid = Array.from({ length: rows }, () => 
-            Array.from({ length: cols }, () => ({
+        this.grid = Array.from({ length: this.rows }, () => 
+            Array.from({ length: this.cols }, () => ({
                 hit:  false,
                 miss: false,
                 ship: null,
@@ -13,6 +13,9 @@ export class GameBoard {
     }
 
     placeShip (ship, x, y) {
+        if (x > this.rows || x < 0) return null;
+        if (y > this.cols || y < 0) return null;
+        if (x + ship.length > this.rows)
         for (let i = 0; i < ship.length; i++) {
             this.grid[x+i][y].ship = ship;
         }
--- src/logic/gameboard/Gameboard.test.js
+++ src/logic/gameboard/Gameboard.test.js
@@ -16,4 +16,25 @@ test('check object placement in cell', () => {
     expect(game.grid[0][0].ship).toBe(newShip);
     expect(game.grid[1][0].ship).toBe(newShip);
     expect(game.grid[2][0].ship).toBe(newShip);
-})
+});
+
+test('x placement inside grid', () => {
+    const newShip = {length: 3};
+    const game = new GameBoard();
+    
+    expect(game.placeShip(newShip, 25, 25)).toBe(null)
+});
+
+test('y placement inside grid', () => {
+    const newShip = {length: 3};
+    const game = new GameBoard();
+    
+    expect(game.placeShip(newShip, 24, 25)).toBe(null)
+});
+
+test('ship out of bounds', () => {
+    const newShip = {length: 3};
+    const game = new GameBoard();
+    
+    expect(game.placeShip(newShip, 23, 21)).toBe(null)
+});
